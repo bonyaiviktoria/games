d30f0b359fc80d4833d1aae1de80d4e70fc0e7a9
Viktória Bonyai <77383172+bonyaiviktoria@users.noreply.github.com>
2023-05-17 13:53:21 +0000

    I've started to code the madlibs game
    It is a funny game with words and stories

---
+++ madlibs/madlibs.py
@@ -0,0 +1,8 @@
+# First declare the needed variables
+exclamation = input("Exclamation: ")
+adverb = input("Adverb: ")
+noun = input("Noun: ")
+adjective = input("Adjective: ")
+
+# This is from wikipedia :)
+print(f"{exclamation}! he said {adverb} as he jumped into his convertible {noun} and drove off with his {adjective} wife")
