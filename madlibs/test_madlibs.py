6e9f0ea42b102009d7576c05b7957feb49bfea26
Viktória Bonyai <77383172+bonyaiviktoria@users.noreply.github.com>
2023-05-22 12:56:57 +0000

    Test program for madlibs is done

--- madlibs/test_madlibs.py
+++ madlibs/test_madlibs.py
@@ -1,4 +1,6 @@
-from madlibs import magic_finder, get_word, magic_replace
+from madlibs import magic_finder
+
 
 def test_default():
-    assert magic_finder("I have {'Number:'} {'Animal: '} at home") == [Number: , Animal: ]
+    assert magic_finder("I have {'Number: '} {'Animal: '} at home") == ["Number: ", "Animal: "]
+    assert magic_finder("There is nothing to do") == None
