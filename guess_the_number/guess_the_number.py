f467129b383f09fdd15c21181adb038cd6871add
Viktória Bonyai <77383172+bonyaiviktoria@users.noreply.github.com>
2023-05-22 08:30:46 +0000

    User guessing

--- guess_the_number/guess_the_number.py
+++ guess_the_number/guess_the_number.py
@@ -1,4 +1,4 @@
-
+import random
 
 def main():
     # The user decides which game to play: Who should guessing?
@@ -8,18 +8,37 @@ def main():
     while game != "user" and game != "computer":
         game = input("The given game type is not valid. Please choose between user or computer")
 
+    # Set the level of the game
+    x = get_number("What should be highest number to find? (The higher the number is, the game gets harder): ")
+
     # Let's play :)
     if game == "user":
-        user_guessing()
+        user_guessing(x)
     elif game == "computer":
-        computer_guessing()
+        computer_guessing(x)
 
 
-def user_guessing()
+def user_guessing(x):
+    # Choose a number randomly betwwen 0 and the given number
+    n = random.randint(0, x)
+    print(n)
 
+    # Greet the user, and prompt for a guess
+
+
+    #
+
+#def computer_guessing(x):
     
 
-
+def get_number(prompt):
+    while True:
+        try:
+            x = int(input(prompt))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            return x
 
 
 if __name__ == "__main__":
