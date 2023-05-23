ec4143a79df15db5f08105915f821c4a376b0851
Viktória Bonyai <77383172+bonyaiviktoria@users.noreply.github.com>
2023-05-23 14:56:47 +0000

    The rock paper sciccors game is done! :D yaaay

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -4,19 +4,8 @@ from random import choice
 options = [{"name": "rock", "sign":"✊"}, {"name": "paper", "sign": "✋"}, {"name": "scissors", "sign": "✌"}]
 
 def main():
-    # Greet the user and prompt to choose
-    user = input("Welcome to the game! Make your choice! (rock/paper/scissors)").lower()
-
-    print(options)
-
-    i = 0
-    for option in options:
-        if option["name"] == user:
-            user = option
-        else:
-            i += 1
-            if i == len(options):
-                print("Your input is not a valid option! Try again.")
+    # Greet the user and prompt to choose. Then transform it into a proper data for us
+    user = get_choice(input("Welcome to the game! Make your choice! (rock/paper/scissors)").lower())
             
     # Choose randomly from the list
     computer = choice(options)
@@ -24,6 +13,23 @@ def main():
     # Print out the battle!
     print(user["sign"], " VS ", computer["sign"])
 
+    if (user["name"] == "rock" and computer["name"] == "scissors") or  (user["name"] == "paper" and computer["name"] == "rock") or (user["name"] == "scissors" and computer["name"] == "paper"):
+        print("Congratulations! 🍾 You are the winner!")
+    elif user["name"] == computer["name"]:
+        print("It's a tie! 🌸")
+    else:
+        print("Sorry, I won this time ✨ Another try?")
+
+
+def get_choice(user_input):
+    i = 0
+    for option in options:
+        if option["name"] == user_input:
+            return option
+        else:
+            i += 1
+            if i == len(options):
+                print("Your input is not a valid option! Try again.") 
 
 
 if __name__ == "__main__":
