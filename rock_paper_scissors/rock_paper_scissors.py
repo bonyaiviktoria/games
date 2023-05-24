78b271aa99d9f1ea53f51bb4da61be4a8a3c9466
Viktória Bonyai <77383172+bonyaiviktoria@users.noreply.github.com>
2023-05-24 04:32:32 +0000

    Redefined the main function, gave a separated battle function to enable testing

--- rock_paper_scissors/rock_paper_scissors.py
+++ rock_paper_scissors/rock_paper_scissors.py
@@ -5,20 +5,13 @@ options = [{"name": "rock", "sign":"✊"}, {"name": "paper", "sign": "✋"}, {"n
 
 def main():
     # Greet the user and prompt to choose. Then transform it into a proper data for us
-    user = get_choice(input("Welcome to the game! Make your choice! (rock/paper/scissors)"))
+    user = get_choice(input("Welcome to the game! Make your choice! (rock/paper/scissors)  "))
             
     # Choose randomly from the list
     computer = choice(options)
 
     # Print out the battle!
-    print(user["sign"], " VS ", computer["sign"])
-
-    if (user["name"] == "rock" and computer["name"] == "scissors") or  (user["name"] == "paper" and computer["name"] == "rock") or (user["name"] == "scissors" and computer["name"] == "paper"):
-        print("Congratulations! 🍾 You are the winner!")
-    elif user["name"] == computer["name"]:
-        print("It's a tie! 🌸")
-    else:
-        print("Sorry, I won this time ✨ Another try?")
+    print(user["sign"], " VS ", computer["sign"], "\n", battle(user, computer))
 
 
 def get_choice(user_input):
@@ -30,6 +23,15 @@ def get_choice(user_input):
             i += 1
             if i == len(options):
                 print("Your input is not a valid option! Try again.") 
+
+
+def battle(user, computer):
+    if (user["name"] == "rock" and computer["name"] == "scissors") or  (user["name"] == "paper" and computer["name"] == "rock") or (user["name"] == "scissors" and computer["name"] == "paper"):
+        return "Congratulations! 🍾 You are the winner!"
+    elif user["name"] == computer["name"]:
+        return "It's a tie! 🌸"
+    else:
+        return "Sorry, I won this time ✨ Another try?"
 
 
 if __name__ == "__main__":
