3115a069326caf794c6b339451d12d202cb400c6
Viktória Bonyai <77383172+bonyaiviktoria@users.noreply.github.com>
2023-05-29 14:11:24 +0000

    guess

--- games/games.py
+++ games/games.py
@@ -1,12 +1,14 @@
 # Import libraries
 import re
-from random import choice
+from random
 
 def main():
     # Which game does the user want to play
     game = input("Which game do you choose to play? (madlibs/guess the number/hangman)")
+
     # Madlibs
     if game == "madlibs":
+
         # Create a python file with a list of the stories. I have to search these around the web
         stories = [
             (
@@ -26,7 +28,7 @@ def main():
             ),
         ]
         # Choose randomly one of the stories, and pass into the finder function to get the list of the words
-        story = choice(stories)
+        story = random.choice(stories)
         words = magic_finder(story)
 
         # Iterate through all of the words
@@ -40,6 +42,28 @@ def main():
         print(story)
 
     # Guess the number
+    elif game == "Guess the number":
+
+        # The user decides which game to play: Who should guessing?
+        game = input("Who should guessing (user/computer)? ").lower()
+
+        # So if the user types in a correct type of game, the function of that game starts. If not, I have to handle that error
+        while game != "user" and game != "computer":
+            game = input(
+                "The given game type is not valid. Please choose between user or computer"
+            )
+
+        # Set the level of the game
+        x = get_number(
+            "What should be the maximum guessable number? (The higher the number you choose, the harder the game will be): "
+        )
+
+        # Let's play :)
+        if game == "user":
+            print(user_guessing(x))
+        elif game == "computer":
+            print(computer_guessing(x))
+
 
     # Hangman
 
@@ -58,6 +82,42 @@ def get_word(word):
             print("Please choose a proper answer")
         else:
             return x.lower()
+
+
+def user_guessing(high):
+    # First I set the borders of the guessing game, and set the counter to 0
+    counter = 0
+
+    # Choose n: a number randomly between 0 and the given number
+    n = random.randint(0, high)
+
+    # Greet the user, and prompt for a guess
+    guess = get_number(
+        "Welcome to the game! I have chosen a number, please make a guess: "
+    )
+
+    # THE GAME
+    while True:
+        # If the guess is bigger than or equal to zero and less than n, set the counter and prompt for a new guess
+        if guess >= 0 and guess < n:
+            counter += 1
+            guess = get_number("The guessed number is too low. Try again: ")
+        # If the guess is less than or equal to zero and bigger than n, set the counter and prompt for a new guess
+        elif guess <= high and guess > n:
+            counter += 1
+            guess = get_number("The guessed number is too high. Try again: ")
+        # If the guess is correct, return the data
+        elif guess == n:
+            return f"Congratulations! You have guessed the number I was thinking of in {counter} attempts, and it was {n}."
+        # If none of the above, there must be a mistake with the boundaries
+        else:
+            counter += 1
+            print(
+                f"This guess is not valid. Please choose a number between 0 and {high}."
+            )
+
+
+
 
 
 if __name__ == "__main__":
